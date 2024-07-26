c3b292d87b276fee2450a0b31f3e48de07905d4c
Rabindra Basnet <rabindrabasnet82@gmail.com>
2024-07-27 02:01:48 +0545

    trianglelearning

---
+++ javascriptday/app.js
@@ -0,0 +1,14 @@
+// stock with the kearning
+
+// loops 
+
+for (let row = 0; row < 5; row++) {
+    let space =" ";
+    let num = 1;
+    for(let cols = 0; cols <= (row); cols++){
+        space+= num;
+        num++;
+
+    }
+    console.log(space);
+}
