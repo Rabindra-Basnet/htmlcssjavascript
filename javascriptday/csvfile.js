dec7ba01408c5e7cbe800f7278c1f7227308cb29
Rabindra Basnet <rabindrabasnet82@gmail.com>
2024-07-30 17:07:38 +0545

    csveditor

--- javascriptday/csvfile.js
+++ javascriptday/csvfile.js
@@ -1,17 +1,21 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const csvWriter = createCsvWriter({
-    path: 'data.csv',
+    path: './data.csv',
     header: [
-        {id: 'name', title: 'Name'},
-        {id: 'age', title: 'Age'},
-        {id: 'city', title: 'City'}
+        {id: 'sn', title: 'SN'},
+        {id: 'contract no', title: 'Contract No.'},
+        {id: 'buyer', title: 'Buyer'},
+        {id: 'seller', title: 'Seller'},
+        {id: 'quantity', title: 'Quantity'},
+        {id: 'rate', title: 'Rate'},
+        {id: 'amount', title: 'Amount'}
     ]
 });
 
 const records = [
-    {name: 'John Doe', age: 30, city: 'New York'},
-    {name: 'Jane Smith', age: 25, city: 'Los Angeles'}
+    {sn: "1", 'contract no':'2024073004000020', buyer:'59', seller:'33', quantity:'50', rate:'690.00', amount:'34,500.00' },
+  
 ];
 
 csvWriter.writeRecords(records)       // returns a promise
